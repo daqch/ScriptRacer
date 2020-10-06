9a269fac209fd2050799477a8378b7f0fde2e518
diego <diego_6525@hotmail.com>
2020-10-06 12:21:50 -0700

    input testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [text, setText] = useState(
+    "A really long test input you can generate. Some times is better to just think about stuuf that happens, and in some cases let it go... "
+  );
+
+  useEffect(() => {}, []);
+
+  const handlechange = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (text.split(" ").length === 1) {
+    }
+
+    if (e.key == " ") {
+      console.log(text);
+      if (text.split(" ")[0] === input.slice(0, input.length - 1)) {
+        setText(text.slice(input.length, text.length));
+        console.log(text);
+        setInput("");
+      }
+    }
+  };
+
+  const updateInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <p>{text}</p>
+      <input
+        onKeyUp={(e) => handlechange(e)}
+        onChange={(e) => {
+          updateInput(e);
+        }}
+        value={input}
+      ></input>
     </div>
   );
 }
